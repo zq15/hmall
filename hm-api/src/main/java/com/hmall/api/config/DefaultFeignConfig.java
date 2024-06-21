ad2ffaee48117bf18c9801bf8a95df87a869985d
zds <1529219484@qq.com>
2024-06-21 10:40:12 +0800

    openfeign 上下文传递

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,11 +1,29 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLogLevel(){
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                // 获取用户信息
+                Long userId = UserContext.getUser();
+                if (userId != null) {
+                    // 将用户信息添加到请求头中
+                    requestTemplate.header("user-info", userId.toString());
+                }
+            }
+        };
     }
 }
